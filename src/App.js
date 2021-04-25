4dbcbf998bca9f0ea350d88680c12614c86c71b5
Anton Danilenko <adanilenko7@gmail.com>
2021-04-25 15:26:07 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,113 +1,36 @@
-import React, { Component } from 'react';
 import Filter from './components/Filter/Filter';
-import ContactForm from './components/ContactForm/ContactForm';
-import ContactsList from './components/ContactsList/ContactsList'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { v4 as uuidv } from 'uuid';
+import ContactForm from './components/ContactForm';
+import ContactsList from './components/ContactsList';
 import { CSSTransition } from 'react-transition-group';
 import Fade from './components/Fade.module.css';
+import { connect } from 'react-redux';
+import contactsActions from './redux/contacts/contacts-action';
 
-export default class App extends Component {
-  state = {
-    contacts: [ ],
-    filter: '',
-    name: '',
-    number: ''
-  };
-
-  componentDidMount() {
-    const oldNum = localStorage.getItem('contact');
-    if (oldNum) {
-      this.setState({
-        contacts: JSON.parse(oldNum)
-      });
-    }
-  };
-  
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState !== this.state.contacts.name) {
-      localStorage.setItem('contact', JSON.stringify(this.state.contacts));
-    }
-  };
-
-  addContact = (e) => {
-    const searchSameName = this.state.contacts
-      .map((contact) => contact.name)
-      .includes(e.name);
-    const notify = () => toast.error(`${e.name} is already in contacts`);
-    const notEmpty = () => toast.error('the field must not be empty!');
-
-    if (searchSameName) {
-      notify();
-    } else if (e.name.length === 0) {
-      notEmpty();
-    } else {
-      const contact = {
-        ...e,
-        id: uuidv(),
-      };
-
-      this.setState((prevState) => ({
-        contacts: [...prevState.contacts, contact],
-      }));
-    }
-  };
-
-  changeFilter = (filter) => {
-    this.setState({ filter });
-  };
-
-  getVisibleContacts = () => {
-    const { contacts, filter } = this.state;
-
-    return contacts.filter((contacts) =>
-      contacts.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  };
-
-  removeContact = (contactId) => {
-    this.setState((prevState) => {
-      return {
-        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
-      };
-    });
-  };
-
-  render() {
-    const { filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-
-    return (
-      <div>
-          <ToastContainer
-            position="top-center"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-          />
-          <CSSTransition
-            in
-            appear={true}
-            timeout={500}
-            classNames={Fade}
-          >
-            <h1>Phonebook</h1>
-          </CSSTransition>
-          <ContactForm onAddContact={this.addContact} />
-          <h2>Contacts</h2>
-          <Filter value={filter} onChangeFilter={this.changeFilter} />
-          <ContactsList
-            contacts={visibleContacts}
-            onRemoveContact={this.removeContact}
-          /> 
-      </div>
-    );
+const App = (props) => {
+  return (
+    <>
+      <CSSTransition
+        in
+        appear={true}
+        timeout={500}
+        classNames={Fade}
+      >
+        <h1>Phonebook</h1>
+      </CSSTransition>
+      <ContactForm onAddContact={props.addContact} />
+      <h2>Contacts</h2>
+      <Filter  />
+      {props.contacts && props.contacts.length > 0 && <ContactsList />} 
+    </>
+  );
   }
-}
+
+const mapStateToProps = (state) => ({
+  contacts: state.contacts.contacts,
+})
+
+const mapDispatchToProps = () => ({
+  addContact: contactsActions.addContact,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,22 +1,33 @@
 import PropTypes from "prop-types";
 import s from './Filter.module.css';
+import { connect } from 'react-redux';
+import contactsActions from '../../redux/contacts/contacts-action';
 
-export default function Filter({ value, onChangeFilter }) {
-  return (
+const Filter = ({ value, onChangeFilter }) => (
     <div className={s.filterContainer}>
       Find contacts by name
       <input
         className={s.filterContainerInput}
         type="text"
         value={value}
-        onChange={(e) => onChangeFilter(e.target.value)}
+        onChange={onChangeFilter}
       />
     </div>
   );
-}
-
 
 Filter.propTypes = {
-  value: PropTypes.string.isRequired,
-  onChangeFilter: PropTypes.func.isRequired,
+  value: PropTypes.string,
+  onChangeFilter: PropTypes.func,
+  contacts: PropTypes.arrayOf(PropTypes.object),
 };
+
+const mapStateToProps = (state) => ({
+  value: state.contacts.filter,
+  contacts: state.contacts.contacts,
+})
+
+const mapDispatchToProps = dispatch => ({
+  onChangeFilter: e => dispatch(contactsActions.changeFilter(e.target.value))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filter);
